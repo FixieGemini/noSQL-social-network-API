7f78092d1f3196f78a3aad0bdb81e7c694b10653
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2022-12-04 13:50:11 -0700

    routes and api folders added containing indexjs thought and user routes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/noSQL-social-media-API', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/noSQL-social-network-API', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
